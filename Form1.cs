1fc479c70e781e28639402ac17c1fb845284963e
MKeru <michaelcwadih@gmail.com>
2022-09-18 17:13:11 -0500

    list all ad libs with text boxes

--- Form1.cs
+++ Form1.cs
@@ -8,14 +8,20 @@ public partial class Form1 : Form
     public Button button1;
     public Form1()
     {
+        this.AutoSize = false;
+
         button1 = new Button();
-        button1.Size = new Size(100, 40);
-        button1.Location = new Point(0, 0);
-        button1.Text = "Select Text File";
+        // button1.Size = new Size(100, 40);
+        button1.Location = new Point(20, 20);
+        button1.Text = "Select New\nText File";
+        // set button width to fit text plus 10 pixels on each side
+        button1.Width = TextRenderer.MeasureText(button1.Text, button1.Font).Width + 20;
+        // set button height to fit text plus 10 pixels on each side
+        button1.Height = TextRenderer.MeasureText(button1.Text, button1.Font).Height + 20;
+
         this.Controls.Add(button1);
         button1.Click += new EventHandler(this.button1_Click);
 
-        this.AutoSize = true;
         InitializeComponent();
     }
 
@@ -56,7 +62,7 @@ public partial class Form1 : Form
                     // find all regex matches in text
                     Regex regex = new Regex("\\[.+?\\]");
                     MatchCollection matches = regex.Matches(rawStory);
-                    
+
                     // create array of size matches
                     string[] adLibs = new string[matches.Count];
                     for (int i = 0; i < matches.Count; i++)
@@ -83,19 +89,39 @@ public partial class Form1 : Form
 
                     // MessageBox.Show(sb.ToString());
 
-                    TextBox newText = new TextBox();
-                    newText.BorderStyle = BorderStyle.FixedSingle;
-                    newText.Size = new Size(100, 20);
-                    var textBoxes = new Dictionary<string, TextBox>();
-
+                    StringBuilder sb = new StringBuilder();
+                    string prettyLabel;
                     for (int i = 0; i < adLibs.Length; i++)
                     {
-                        newText.Text = adLibs[i];
-                        newText.Location = new Point(20, (i * 40) + 50);
-                        textBoxes.Add(adLibs[i], newText);
+                        prettyLabel = adLibs[i];
+                        // capitalize first letter and add colon to end
+                        adLibs[i] = prettyLabel.Substring(0, 1).ToUpper() + prettyLabel.Substring(1) + ": ";
                     }
 
+                    TextBox newText = new TextBox();
+                    // create panel with text boxes
+                    var panel = new TableLayoutPanel();
 
+                    var adLibFieldEntry = new adLibField(adLibs[0], new TextBox());
+                    panel.Controls.Add(adLibFieldEntry);
+
+                    for (int i = 1; i < adLibs.Length; i++)
+                    {
+                        adLibFieldEntry = new adLibField(adLibs[i], new TextBox());
+                        panel.Controls.Add(adLibFieldEntry);
+                    }
+
+                    // set panel size to fit all text boxes
+                    panel.AutoSize = true;
+                    // allow form to scroll
+                    this.AutoScroll = true;
+                    // set panel location to right of button1
+                    panel.Location = new Point(button1.Location.X + button1.Width + 20, button1.Location.Y);
+                    // add scroll bar to panel
+                    // add visual boundary to panel
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+
+                    this.Controls.Add(panel);
                 }
             }
             catch (Exception)
@@ -104,3 +130,19 @@ public partial class Form1 : Form
         }
     }
 }
+
+class adLibField : TableLayoutPanel
+    {
+        public Label adLibLabel;
+        public adLibField(string label, TextBox textBox)
+            : base()
+        {
+            AutoSize = true;
+            adLibLabel = new Label();
+            adLibLabel.Text = label;
+            adLibLabel.AutoSize = true;
+
+            Controls.Add(adLibLabel);
+            Controls.Add(textBox);
+        }
+    }
