99b6b45872a16dadc429b4c2106bfb1879fc032b
MKeru <michaelcwadih@gmail.com>
2022-09-28 21:09:04 -0500

    AdLibField added

---
+++ AdLibField.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+public class AdLibField
+{
+    public Label label;
+    public TextBox textBox;
+    public AdLibField(string labelText)
+    {
+        label = new Label();
+        label.Content = labelText;
+        textBox = new TextBox();
+    }
+}
